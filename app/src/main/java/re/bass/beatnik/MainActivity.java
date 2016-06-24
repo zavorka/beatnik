6dc2c8a6581be245b6c0209e9f6097c80ecf2a47
Curly Bracket <romanberanek@icloud.com>
2016-06-24 06:16:05 +0200

    Display that fakin bpm

--- app/src/main/java/re/bass/beatnik/MainActivity.java
+++ app/src/main/java/re/bass/beatnik/MainActivity.java
@@ -2,8 +2,13 @@ package re.bass.beatnik;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity
+{
+    private final static String TAG = "MainActivity";
 
     // private final static ... how could you possibly not love Java?
     private final static int SAMPLE_RATE = 44100;
@@ -15,10 +20,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final TextView textView = (TextView)findViewById(R.id.text_view);
+
         Microphone microphone = new Microphone(SAMPLE_RATE, STEP_SIZE);
-        microphone.addListener(new DFAudioProcessor(SAMPLE_RATE, STEP_SIZE, WINDOW_SIZE));
+        DFAudioProcessor processor = new DFAudioProcessor(SAMPLE_RATE, STEP_SIZE, WINDOW_SIZE);
+        BeatAnalyzer analyzer = new BeatAnalyzer(SAMPLE_RATE, STEP_SIZE, WINDOW_SIZE);
+
+        microphone.addListener(processor);
+        processor.addOnProcessorOutputListener(analyzer);
+        analyzer.addOnBPMCalculatedListener(new BeatAnalyzer.OnBPMCalculatedListener() {
+            @Override
+            public void onBPMCalculated(final float bpm) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        assert textView != null;
+                        textView.setText(getString(R.string.bpm_value, bpm));
+                    }
+                });
+            }
+        });
 
         microphone.start();
+        analyzer.start();
     }
 
     // Used to load the 'native-lib' library on application startup.
