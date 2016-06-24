046940e823ffae36f87ad113d6cb17c44b73c8ce
Curly Bracket <romanberanek@icloud.com>
2016-06-24 02:28:52 +0200

    AudioReceiver renamed to AudioInput and onStart notification added

--- app/src/main/java/re/bass/beatnik/AudioInput.java
+++ app/src/main/java/re/bass/beatnik/AudioInput.java
@@ -6,7 +6,7 @@ import java.nio.ByteBuffer;
  * Created by curly on 22/06/2016.
  */
 
-interface AudioReceiver extends Runnable {
+interface AudioInput extends Runnable {
     void addListener(AudioListener listener); // no pun intended
     void removeListener(AudioListener listener);
 
@@ -16,5 +16,6 @@ interface AudioReceiver extends Runnable {
 
     interface AudioListener {
         void onAudio(ByteBuffer buffer);
+        void onStart();
     }
 }
